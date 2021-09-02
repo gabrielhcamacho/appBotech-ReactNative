4236468027da10c037933dbaf766f159f2bd81c3
Gabriel Camacho <gabrielh.camacho@outlook.com>
2021-09-01 21:16:31 -0300

    Explore page grid

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,13 +1,151 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 
 import {
-    Container
+    Container,
+    Header,
+    PageTitle,
+    SearchBarContainer,
+    Icon,
+    TextInput,
+    Body,
+    Title,
+    Row,
+    TechContainer,
+    TechHeader,
+    TechTitle,
+    TechDescription,
 } from './styles'
 
 export function Explore(){
     return(
         <Container>
 
+            <Header>
+                <PageTitle>Explore</PageTitle>
+                <SearchBarContainer>
+                    <Icon/>
+                    <TextInput 
+                        placeholder="search" 
+                        placeholderTextColor='#FFF'></TextInput>
+                </SearchBarContainer>
+            </Header>
+
+
+            <Body>
+                <ScrollView>
+                    <Title>FIND CLUBS AND CONTENT ABOUT...</Title>
+
+                    <Row>
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+                    </Row>
+
+                    <Row>
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+                    </Row>
+
+                    <Row>
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+                    </Row>
+
+                    <Row>
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+                    </Row>
+
+                    <Row>
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+                    </Row>
+                    
+                    <Row>
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+
+                        <TechContainer>
+                            <TechHeader>
+                                <Icon/>
+                                <TechTitle></TechTitle>
+                            </TechHeader>
+                            <TechDescription></TechDescription>
+                        </TechContainer>
+                    </Row>
+
+                </ScrollView>
+            </Body>
         </Container>
     )
 }
--- src/pages/Explore/styles.ts
+++ src/pages/Explore/styles.ts
@@ -1,6 +1,102 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
     flex: 1;
-    background-color: ${({theme}) => theme.colors.background}
+    background-color: ${({theme}) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+    padding: ${RFValue(0)}px ${RFValue(30)}px;
+    width: 100%;
+    height: ${RFValue(75)}px;
+
+    align-items: center;
+    justify-content: center;
+
+    padding-bottom: 19px;
+`;
+
+export const PageTitle = styled.Text`
+    color: ${({theme})=> theme.colors.text};
+    font-size: ${RFValue(20)}px;
+    font-weight: 600;
+
+    margin-top: ${RFValue(10)}px;
+`;
+
+export const SearchBarContainer = styled.View`
+    margin-top: ${RFValue(15)}px;
+    padding: ${RFValue(10)}px;
+
+    background-color: #3D4255;
+
+    width: 100%;
+    height: ${RFValue(35)}px;
+    border-radius: ${RFValue(10)}px;
+
+    justify-content: center;
+`;
+
+export const Icon = styled.View`
+
+`;
+
+export const TextInput = styled.TextInput`
+    font-size: ${RFValue(16)}px;
+    color: ${({theme})=> theme.colors.text};
+`;
+
+export const Body = styled.View`
+
+`;
+
+export const Title = styled.Text`
+    color: ${({theme})=> theme.colors.text};
+    font-size: ${RFValue(15)}px;
+    font-weight: 500;
+
+    margin-top: ${RFValue(15)}px;
+    padding-left: ${RFValue(30)}px;
+`;
+
+export const Row = styled.View`
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+
+    margin-top: ${RFValue(0)}px;
+    padding: ${RFValue(0)}px ${RFValue(10)}px;
+    
+    background-color: #3D4255;
+
+    width: 100%;
+    height: ${RFValue(120)}px;
+`;
+
+export const TechContainer = styled.View`
+    margin-top: ${RFValue(15)}px;
+    
+    padding: ${RFValue(10)}px;
+
+    margin-right: 15px;
+
+    background-color: ${({theme})=> theme.colors.background};
+
+    width: 48%;
+    height: ${RFValue(100)}px;
+
+    border-radius: ${RFValue(7)}px;
+`;
+
+export const TechHeader = styled.View`
+
+`;
+
+export const TechTitle = styled.Text`
+
+`;
+
+export const TechDescription = styled.Text`
+
 `;
